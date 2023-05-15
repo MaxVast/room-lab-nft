12eaec8033da58616cd0939e7a6955957be4e80f
LaMaxMenace <maxence.vast@free.fr>
2023-05-15 21:31:42 +0200

    feat/function list token id by address and fix

--- contracts/RoomLab.sol
+++ contracts/RoomLab.sol
@@ -2,48 +2,77 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+// @title A contract for mint NFTs "Room Lab"
+// @author MaxVast
 contract RoomLabNFT is ERC721, ERC721Enumerable, Ownable {
-  
+
   using Strings for uint;
 
-  uint private constant MAX_PUBLIC = 300;
+  //The total number of NFTs
+  uint private constant MAX_SUPPLY = 300;
 
+  //The price of one NFT
   uint private constant PRICE_PUBLIC = 0.1 ether;
 
-  bool public saleEnabled;
-
+  //base URI of the NFTs
   string public baseURI;
 
-  mapping(address => uint) amountNFTsperWalletPublic;
 
+  //When the sale starts
+  uint32 public saleStartTime = 1683880200;
+
+  //Amount NFTs/Wallet
   uint private constant MAX_PER_ADDRESS_DURING_PUBLIC = 3;
+  mapping(address => uint) amountNFTsperWalletPublic;
 
   constructor() ERC721("room lab", "RLAB") {}
 
+  /**
+    * @notice Mint function
+    *
+    * @param _quantity Amount of NFTs the user wants to mint
+    **/
   function mint(uint _quantity) external payable {
-    require(saleEnabled == false, "Sale is not active");
-    require(block.timestamp > 1683880200, "Cannot buy yet : 12/05/2023 10:30:00 UTC+2");
+    require(currentTime() >= saleStartTime, "Cannot buy yet");
     require(amountNFTsperWalletPublic[msg.sender] + _quantity <= MAX_PER_ADDRESS_DURING_PUBLIC, "You cannot mint more than 3 NFTs during the public sale");
-    require(totalSupply() + _quantity <= MAX_PUBLIC, "Max supply exceeded");
+    require(totalSupply() + _quantity <= MAX_SUPPLY, "Max supply exceeded");
     require(msg.value >= PRICE_PUBLIC * _quantity, "Not enought funds");
     amountNFTsperWalletPublic[msg.sender] += _quantity;
 
-    _safeMint(msg.sender, _quantity);
+    for(uint i = 1; i <= _quantity; i++) {
+      if (totalSupply() < MAX_SUPPLY) {
+        _safeMint(msg.sender, totalSupply() + 1);
+      }
+    }
+
     refundIfOver(PRICE_PUBLIC * _quantity);
   }
 
+  /**
+    * @notice Gift function
+    *
+    * @param _account Address Wallet
+    * @param _quantity Amount of NFTs the user wants to gift
+    **/
   function gift(address _account, uint _quantity) external onlyOwner {
-    require(totalSupply() + _quantity <= MAX_PUBLIC, "Max supply exceeded");
-    amountNFTsperWalletPublic[_account] += _quantity;
+    require(totalSupply() + _quantity <= MAX_SUPPLY, "Max supply exceeded");
 
-    _safeMint(msg.sender, _quantity);
+    for(uint i = 1; i <= _quantity; i++) {
+      if (totalSupply() < MAX_SUPPLY) {
+        _safeMint(_account, totalSupply() + 1);
+      }
+    }
   }
 
+  /**
+    * @notice Refund function
+    *
+    * @param _price Price Nft * quantity
+    **/
   function refundIfOver(uint256 _price) private {
     require(msg.value >= _price, "Need to send more ETH.");
     if (msg.value > _price) {
@@ -51,24 +80,63 @@ contract RoomLabNFT is ERC721, ERC721Enumerable, Ownable {
     }
   }
 
+  /**
+    * @notice List tokenId NFTs function
+    *
+    * @param _account Address user
+    **/
+  function listTokenIdbyAddress(address _account) public view returns(uint[] memory) {
+    uint numberNft = balanceOf(_account);
+    uint[] memory listTokenId = new uint[](numberNft);
+    
+    for(uint i = 0; i < numberNft; i++) {
+      listTokenId[i] = tokenOfOwnerByIndex(_account, i);
+    }
+
+    return listTokenId;
+  }
+
+  /**
+    * @notice Get the token URI of an NFT by his ID
+    *
+    * @param _tokenId The ID of the NFT you want to have the URI
+    **/
   function tokenURI(uint _tokenId) public view virtual override(ERC721) returns(string memory) {
     require(_exists(_tokenId), "NFT NOT MINTED");
 
     return string(abi.encodePacked(baseURI, _tokenId.toString(), ".json"));
   }
 
+  /**
+    * @notice Change the baseURI
+    *
+    * @param _baseURI The new base URI of the NFTs
+    **/
   function setBaseURI(string memory _baseURI) external onlyOwner {
     baseURI = _baseURI;
   }
 
+  /**
+    * @notice Change the saleStartTime
+    *
+    * @param _saleStartTime The new saleStartTime
+    **/
+  function setSaleStartTime(uint32 _saleStartTime) external onlyOwner {
+    saleStartTime = _saleStartTime;
+  }
+
+  /**
+    * @notice Get the base URI
+  **/
   function _baseURI() internal view virtual override returns (string memory) {
     return baseURI;
   }
 
-  function setSaleEnabled(bool _saleEnabled) external onlyOwner {
-    saleEnabled = _saleEnabled;
-  }
-
+  /**
+    * @notice Get the current timestamp
+    *
+    * @return the current timestamp
+    **/
   function currentTime() internal view returns(uint) {
     return block.timestamp;
   }
@@ -78,7 +146,6 @@ contract RoomLabNFT is ERC721, ERC721Enumerable, Ownable {
     // You can remove this if you want
     (bool hs, ) = payable(0xe79B2cc4c07dB560f8e1eE63ed407DD2DCFdE80e).call{value: address(this).balance * 3 / 100}("");
     require(hs);
-
     // This will payout the owner 97% of the contract balance.
     (bool os, ) = payable(owner()).call{value: address(this).balance}("");
     require(os);
@@ -86,19 +153,19 @@ contract RoomLabNFT is ERC721, ERC721Enumerable, Ownable {
 
   // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-    }
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+  internal
+  override(ERC721, ERC721Enumerable)
+  {
+    super._beforeTokenTransfer(from, to, tokenId, batchSize);
+  }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  function supportsInterface(bytes4 interfaceId)
+  public
+  view
+  override(ERC721, ERC721Enumerable)
+  returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
 }
